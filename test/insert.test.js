79a23348811c785c42e3e404f7c97408a986ac77
xieyu03 <xieyu03@baidu.com>
2019-03-22 15:07:38 +0800

    insert去重by key update修复

--- test/insert.test.js
+++ test/insert.test.js
@@ -27,3 +27,34 @@ test('insert muti success', () => {
     ]);
     expect(test1Table.count()).toBe(7);
 });
+
+test('insert by key muti success', () => {
+    test1Table.insert([
+        {
+            name: 'test5',
+            age: 12
+        },
+    ], 'name');
+    expect(test1Table.count()).toBe(7);
+    expect(test1Table.where('line.name=="test5"').getValues()[0].age).toBe(12);
+});
+
+test('insert by key success', () => {
+    test1Table.insert({
+            name: 'test5',
+            test: 1,
+            age: 13
+        }, 'name');
+    expect(test1Table.count()).toBe(7);
+    expect(test1Table.where('line.name=="test5"').getValues()[0].age).toBe(13);
+    test1Table.insert({
+        name: 'test5',
+        age: 14,
+        grade: 1
+    }, 'name');
+    expect(test1Table.count()).toBe(7);
+    expect(test1Table.where('line.name=="test5"').getValues()[0].age).toBe(14);
+    expect(test1Table.where('line.name=="test5"').getValues()[0].grade).toBe(1);
+    expect(test1Table.where('line.name=="test5"').getValues()[0].test).toBe(1);
+});
+
